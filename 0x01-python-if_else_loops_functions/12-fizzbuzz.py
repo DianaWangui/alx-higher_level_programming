e0ae76040395f89d6d63e2fb7dec6dee6683aaed
DianaWangui <dianawanguikt048@gmail.com>
2023-10-31 23:59:17 +0300

    Fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,9 +3,9 @@ def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
-        if i % 3 == 0:
+        elif i % 3 == 0:
             print("Fizz", end=" ")
-        if i % 5 == 0:
+        elif i % 5 == 0:
             print("Buzz", end=" ")
         else:
             print(i, end=" ")
