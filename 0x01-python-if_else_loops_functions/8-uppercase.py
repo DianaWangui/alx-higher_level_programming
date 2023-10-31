23d7fbd1eb1529a696f4caf15c0feba4a2fb09af
DianaWangui <dianawanguikt048@gmail.com>
2023-10-31 22:38:58 +0300

    Printitng string in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        character = ord(char)
+        if character >= 97 and character <= 122:
+            character -= 32
+        print("{}".format(chr(character)), end=" ")
+    print()
