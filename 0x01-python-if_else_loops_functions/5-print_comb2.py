6f2287feadbaa61cbfc03b972cb808e53628d2f7
DianaWangui <dianawanguikt048@gmail.com>
2023-10-31 18:07:06 +0300

    A code that prints 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 for i in range(100):
     if i < 10:
-       formatted_i = "0{:d}".format(i)
+        formatted_i = "0{:d}".format(i)
     else:
-       formatted_i = "{:d}".format(i)
+        formatted_i = "{:d}".format(i)
     if i == 99:
         print(formatted_i)
     else:
