2aaa15d74b44dab9669c99b6b70aac1ef22bf5e0
DianaWangui <dianawanguikt048@gmail.com>
2023-11-01 00:23:01 +0300

    Priting ascii in reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('A') -1, -1):
+for i in range(ord('z'), ord('A') - 1, - 1):
     if ord('a') <= i <= ord('z'):
         print("{:c}".format(i), end="")
+    elif ord('A') <= i <= ord('Z'):
+        print(("{:c}".format(i), end="")
 print()
