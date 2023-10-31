28b8792af95074c68f02ee80e582a5e1b70160e1
DianaWangui <dianawanguikt048@gmail.com>
2023-11-01 01:02:19 +0300

    singly list for c

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,6 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -11,8 +14,8 @@
  */
 typedef struct listint_s
 {
-	    int n;
-	        struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
