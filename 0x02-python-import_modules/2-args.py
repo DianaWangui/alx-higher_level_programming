c37ba9e795c098ef468bc81bef309effd2164294
DianaWangui <dianawanguikt048@gmail.com>
2023-11-02 18:54:49 +0300

    line arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,13 @@
 if __name__ = '__main__':
     import sys
 
-    count = len(sys.argv) - 1  # Minus the file name argument
+    count = len(sys.argv) - 1
     if count == 0:
-        print("{} arguments.".format('0')
+        print("{} arguments.".format('0'))
     elif count == 1:
         print("{} argument:".format(count))
     else:
-        print("{} arguments:".format(count))
+        print("{} argument:".format(count))
+
     for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
