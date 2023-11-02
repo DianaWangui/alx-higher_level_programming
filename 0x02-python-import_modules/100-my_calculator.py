744b1eac8fd15c66ad3de5eeed41344c4acdc258
DianaWangui <dianawanguikt048@gmail.com>
2023-11-02 23:48:47 +0300

    correcting calculator error

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,12 +2,12 @@
 import sys
 from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-    arguments = len(sys.argv) - 1
-    if arguments != 3:
+    arguments = len(sys.argv)
+    if arguments != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     a = int(sys.argv[1])
-    operator = sys.arg[2]
+    operator = sys.argv[2]
     b = int(sys.argv[3])
 
     if operator == '+':
