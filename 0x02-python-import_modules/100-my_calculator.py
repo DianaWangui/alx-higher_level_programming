0227ad6c2582ca1cc412e3bde699a77be2ec618e
DianaWangui <dianawanguikt048@gmail.com>
2023-11-02 23:29:43 +0300

    suilding my own calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    import sys
-    from calculator_1 import add, sub, mul, div
+import sys
+from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
     arguments = len(sys.argv)
     if arguments != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
