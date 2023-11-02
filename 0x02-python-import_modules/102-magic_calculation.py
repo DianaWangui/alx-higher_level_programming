bb0f52f93d111230acb0abbf08266f3fe15a1595
DianaWangui <dianawanguikt048@gmail.com>
2023-11-03 00:06:58 +0300

    indentation error

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,8 +3,8 @@ def magic_calculator(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
-       for i in range(4, 6):
-           c = (c, i)
+        for i in range(4, 6):
+            c = (c, i)
         return (c)
     else:
         return(sub(a, b))
