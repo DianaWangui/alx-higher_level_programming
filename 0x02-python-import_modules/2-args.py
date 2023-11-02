e6cd6ce213b8962067058ee36510282585c6c618
DianaWangui <dianawanguikt048@gmail.com>
2023-11-02 18:57:36 +0300

    line arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     elif count == 1:
         print("{} argument:".format(count))
     else:
-        print("{} argument:".format(count))
+        print("{} arguments:".format(count))
 
     for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
