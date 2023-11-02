823ac010d3b5bcd122a78617ddcdb5c49bb18ac7
DianaWangui <dianawanguikt048@gmail.com>
2023-11-02 18:40:36 +0300

    line arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ = '__main__':
 
     arg_count = len(sys.argv) - 1  # Minus the file name argument
     if arg_count == 0:
-        print("0 arguments."
+        print("0 arguments.")
     elif arg_count == 1:
         print("{} argument:".format(arg_count))
     else:
