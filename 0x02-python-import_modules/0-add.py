c59cea83b69964f2e38a81d3c27f0215c04bddb9
DianaWangui <dianawanguikt048@gmail.com>
2023-11-02 17:22:41 +0300

    Correcting error

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,4 +3,5 @@ if __name__ == '__main__':
     from add_0 import add
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    result = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, result))
