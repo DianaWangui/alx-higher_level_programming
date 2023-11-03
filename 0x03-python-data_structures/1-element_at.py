946bbdc5a8801c1fd7fc9e67c752bf067fb22d19
DianaWangui <dianawanguikt048@gmail.com>
2023-11-03 18:52:20 +0300

    Retrieving elements from a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+my_list = [2, 4, 5, 7]
+idx = 4
+
+def element_at(my_list, idx):
+    listlen = len(my_list)
+    idx_len = listlen - 1
+    if idx < 0:
+        return None
+    elif idx > idx_len:
+        return None
+    else:
+        return my_list[idx]
+result = element_at(my_list, idx)
+if result is not None:
+    print("Element at index {:d} is {}".format(idx, result))
