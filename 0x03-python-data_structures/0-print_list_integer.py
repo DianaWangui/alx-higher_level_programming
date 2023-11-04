24793d9b4c6cb3ed3545fb5bb624b8c7ea1e03c0
DianaWangui <dianawanguikt048@gmail.com>
2023-11-04 12:05:57 +0300

    Correcting pycode error, removing test codes

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-my_list = [1,2,3]
 def print_list_integer(my_list=[]):
     list_len = len(my_list)
     for i in my_list:
         print("{}".format(i))
-print_list_integer(my_list)
