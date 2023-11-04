e13929917c560887d685bd33b66d249039ce0847
DianaWangui <dianawanguikt048@gmail.com>
2023-11-04 12:55:53 +0300

    function that replaces element in a list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,8 @@ def new_in_list(my_list, idx, element):
     list_len = len(my_list)
     idx_len = list_len - 1
     if idx < 0 or idx > idx_len:
-        return my_list
+        return my_list[:]
     else:
-        my_list[idx] = element
+        new_list = my_list[:]
+        new_list[idx] = element
         print(my_list)
