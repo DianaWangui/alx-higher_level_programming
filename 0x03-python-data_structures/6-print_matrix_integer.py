273309f27dbdab4b0c101481a2780e018c5535b8
DianaWangui <dianawanguikt048@gmail.com>
2023-11-05 11:41:34 +0300

    Nested matrix printing

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    matrix_len = len(matrix)
+    col = 0
     for row in matrix:
+        items = len(row) - 1
         for number in row:
-            print("{}".format(number), end=" ")
+            if col < items:
+                print("{:d}".format(number), end=" ")
+            else:
+                print("{:d}".format(number), end="")
+            col += 1
         print()
