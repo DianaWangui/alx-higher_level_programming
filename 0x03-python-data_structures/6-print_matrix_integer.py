446d6c98bb21733a293caf60227aa9f659a53402
DianaWangui <dianawanguikt048@gmail.com>
2023-11-05 11:48:23 +0300

    Correcting some more edge cases

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,7 +6,7 @@ def print_matrix_integer(matrix=[[]]):
         items = len(row)
         column = 0
         for number in row:
-            if column < items - 1
+            if column < items - 1:
                 print("{:d}".format(number), end=" ")
             else:
                 print("{:d}".format(number), end="")
