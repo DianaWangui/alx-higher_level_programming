c55d7d945c7af0bb2eca2bc513273f2f29b172c3
DianaWangui <dianawanguikt048@gmail.com>
2023-11-05 12:37:52 +0300

    Correcting one checker error

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,6 +3,6 @@ def multiple_returns(sentence):
     length = len(sentence)
     first = sentence[0]
     if length == 0:
-        return length, None
+        return (0, None)
     else:
-        return length, first
+        return (length, first)
