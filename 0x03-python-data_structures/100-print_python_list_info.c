5b7b70da51c126c216e3643bc7eeafa168c5ef04
DianaWangui <dianawanguikt048@gmail.com>
2023-11-06 14:26:57 -0800

    Betty errors

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -10,12 +10,11 @@ void print_python_list_info(PyObject *p)
 {
 	long int size = PyList_Size(p);
 	int i;
-	
 	PyListObject *obj = (PyListObject *)p;
 
 	printf("[*] Size of the Python List = %li\n", size);
 	printf("[*] Allocated = %li\n", obj->allocated);
 
-	for (i = 0; i < size; i+=)
+	for (i = 0; i < size; i++)
 		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
 }
