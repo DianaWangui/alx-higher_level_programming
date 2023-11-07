cf277fd3042c13edcd7c88703c17e07b45bdf59c
DianaWangui <dianawanguikt048@gmail.com>
2023-11-07 09:13:03 +0300

    Correcting some compilation errors

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first = sentence[0]
     if length == 0:
         return (0, None)
     else:
-        no_empty = (length, first)
-        return (no_empty)
+        no_empty = (length, sentence[0])
+        return no_empty
