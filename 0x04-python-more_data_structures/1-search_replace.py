3dd4786c81c630e265bf7f64c0fd85093df84ab1
DianaWangui <dianawanguikt048@gmail.com>
2023-11-09 00:43:41 +0300

    a function that replaces occurrence of a an element by another element

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = []
+    for element in my_list:
+        if element == search:
+            new_list.append(replace)
+        else:
+            new_list.append(element)
+    return new_list
