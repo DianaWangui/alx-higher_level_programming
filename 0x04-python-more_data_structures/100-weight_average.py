78fb8e043a8220e8fc8c3e26721f6e9fb457d419
DianaWangui <dianawanguikt048@gmail.com>
2023-11-09 19:42:40 +0300

    function that returns weighted average of all uintegers tuple

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 ef weight_average(my_list=[]):
-    if my_list and len(my_list):
-        number = 0
-        d_num = 0
-        for tup in my_list:
-            number += (tup[0] * tup[1])
-            d_num += (tup[1])
-        return (number/d_num)
-    return 0
+    if not my_list:
+        return 0
+    number = 0
+    d_num = 0
+    for tup in my_list:
+        number += (tup[0] * tup[1])
+        d_num += (tup[1])
+    return (number/d_num)
