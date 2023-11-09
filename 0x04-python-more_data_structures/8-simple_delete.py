b21b2942d9f38645aca2c9933d8347e6ac757e3f
DianaWangui <dianawanguikt048@gmail.com>
2023-11-09 18:24:22 +0300

    function that deletes a key in a dic

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    del a_dictionary[key]
-    return a_dictionary
+    dic_dup = a_dictionary.copy()
+    if key in a_dictionary:
+        del a_dictionary[key]
+        return a_dictionary
+    else:
+        return dic_dup
