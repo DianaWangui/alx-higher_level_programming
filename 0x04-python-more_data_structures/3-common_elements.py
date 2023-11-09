e5406bee78e8b96cb3b4005d9141cf0c56e5b909
DianaWangui <dianawanguikt048@gmail.com>
2023-11-09 11:44:19 +0300

    A fucntion that prints common element in two sets

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    c_set = []
-    for i in set_1:
-        if i in set_2:
-            result = i
+    result = [i for i in set_1 if i in set_2]
     return result
