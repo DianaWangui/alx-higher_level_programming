5aaf636d7b4ae920f6e58785653beda428165e41
DianaWangui <dianawanguikt048@gmail.com>
2023-11-20 19:24:05 +0300

    checker error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,3 +10,13 @@ def safe_print_list(my_list=[], x=0):
     finally:
         print()
     return nb_print
+
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
