f1d9d23041124c923b87f3840459408a301b8167
DianaWangui <dianawanguikt048@gmail.com>
2023-11-20 19:26:15 +0300

    wooh really why did i put space in my end

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     nb_print = 0
     try:
         for i in range(x):
-            print("{}".format(my_list[i]), end=" ")
+            print("{}".format(my_list[i]), end="")
             nb_print += 1
     except IndexError:
         pass
