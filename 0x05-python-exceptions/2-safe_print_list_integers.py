ca5a957d178d9e65cd8611295e061a050c666304
DianaWangui <dianawanguikt048@gmail.com>
2023-11-20 23:57:55 +0300

    printing values of a function

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+def is_int(value):
+    try:
+        int(value)
+        return True
+    except Value Error:
+        return False
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    nb_print = 0
+    try:
+        for i in range(x):
+            if not i.is_int(i):
+                continue
+            print("{:d}".format(my_list[i]), end="")
+            nb_print += 1
+    except ValueError:
+        pass
+    finally:
+        print()
+    return nb_print
+
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list_integers(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+
+my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+nb_print = safe_print_list_integers(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
