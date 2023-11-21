070c5738ad92e7a053b6af7754df6232470907e6
DianaWangui <dianawanguikt048@gmail.com>
2023-11-21 22:01:55 +0300

    return false lol

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 import sys
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
+
     except (ValueError, TypeError) as error:
         print("Exception: {}".format(error), file=sys.stderr)
         return False
