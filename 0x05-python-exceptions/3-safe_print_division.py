d34838b36c8fe2a485a9e67a7c5eef6a3efb55c6
DianaWangui <dianawanguikt048@gmail.com>
2023-11-21 09:34:25 +0300

    function that divides 2 integers and prints the result.

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -8,15 +8,3 @@ def safe_print_division(a, b):
     finally:
         print("Inside result: {}".format(div))
     return div
-        
-
-
-a = 12
-b = 2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
-
-a = 12
-b = 0
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
