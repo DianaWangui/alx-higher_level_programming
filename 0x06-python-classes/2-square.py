125c5a6bc84255f84c424b7fbdff8d95ddc038fa
DianaWangui <dianawanguikt048@gmail.com>
2023-11-23 22:42:18 +0300

    correcting pycode error

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,8 +8,6 @@ class Square:
     raising errors incase condition fails
     """
     def __init__(self, size=0):
-       # self.__size = 0
-        # self.__size = size
 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
