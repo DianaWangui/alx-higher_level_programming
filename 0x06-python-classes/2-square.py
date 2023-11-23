adc2e892a5a0fb8dc458ceb44fcf6fdb1026718c
DianaWangui <dianawanguikt048@gmail.com>
2023-11-23 22:39:24 +0300

    try

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,8 +8,8 @@ class Square:
     raising errors incase condition fails
     """
     def __init__(self, size=0):
-        self.__size = 0
-        self.__size = size
+       # self.__size = 0
+        # self.__size = size
 
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
