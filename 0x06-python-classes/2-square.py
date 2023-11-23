48c4211196ecf63e09489849d9fa1f3527933337
DianaWangui <dianawanguikt048@gmail.com>
2023-11-23 22:23:06 +0300

    a class Square that defines a square by: (based on 1-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+
+class Square:
+    def __init__(self, size=0):
+        self.__size = 0
+        self.__size = size
+    @property
+    def size(self):
+        return self.__size
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+
+my_square_1 = Square(3)
+print(type(my_square_1))
+print(my_square_1.__dict__)
+
+my_square_2 = Square()
+print(type(my_square_2))
+print(my_square_2.__dict__)
+
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_3 = Square("3")
+    print(type(my_square_3))
+    print(my_square_3.__dict__)
+                
+except Exception as e:
+    print(e)
+
+try:
+    my_square_4 = Square(-89)
+    print(type(my_square_4))
+    print(my_square_4.__dict__)
+except Exception as e:
+    print(e)
