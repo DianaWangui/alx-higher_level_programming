e033d9465eb550a3ae519332e118f8bbf78f9c5e
DianaWangui <dianawanguikt048@gmail.com>
2023-11-23 22:59:12 +0300

    style errors

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-
-""" 
+"""
     a class square that defines a square
     with private instances of size and raisng
     error incase certain conditions are not met
+
 """
 
+
 class Square:
-    """
-        instatiating object attribute size 
-    """
+    """instatiating object attribute size"""
     def __init__(self, size=0):
 
         if not isinstance(size, int):
@@ -18,6 +17,7 @@ class Square:
             raise ValueError("size must be >=0")
         else:
             self.__size = size
+
     def area(self):
         sArea = int(self.__size) * int(self.__size)
     return area.sArea
