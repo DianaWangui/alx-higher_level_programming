ad9b907a3a10e8f9e2eaf1210a11a6ffffc00aad
DianaWangui <dianawanguikt048@gmail.com>
2023-11-25 00:19:14 +0300

    pycode errors

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 """
 class Square that defines a square
 The class takes one private instance size
@@ -10,16 +11,15 @@ check condition of the value and raises error
 
 
 class Square:
+
     """ instantiating private attribute size using __"""
     def __init__(self, size=0):
         self.__size = size
 
-    # getting the size using property decorater
     @property
     def size(self):
         return self.__size
 
-    # setting the value and raising error on some conditions
     @size.setter
     def size(self, value):
         if isinstance(value, int):
@@ -29,11 +29,9 @@ class Square:
         else:
             self.__size = value
 
-    # a function to find area of the square
     def area(self):
         return (self.__size ** 2)
 
-    # a function to print to stdout the square using #
     def print(self):
         if self.__size = 0:
             print()
