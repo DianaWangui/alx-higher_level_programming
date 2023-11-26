cb6ccb2559815ada1dafd237aa0de1754f2c85a0
DianaWangui <dianawanguikt048@gmail.com>
2023-11-27 00:35:09 +0300

    pycode error

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -24,8 +24,8 @@ class Square:
     printing sqaure using '#'
     """
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
