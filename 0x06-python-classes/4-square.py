a224158fa5ff35ea27ee841a1c185730e5c5ea30
DianaWangui <dianawanguikt048@gmail.com>
2023-11-27 00:40:33 +0300

    checker error

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
 
-""" a square class that defines a square """
+""" 
+a square class that defines a square
+And a Public instance method: def area(self):
+that returns the current square area
+Methods Getter and Setter properties for size.
+property def size(self): to retrieve it
+property setter def size(self, value): to set it:
+"""
 
 
 class Square:
-    """instantiation of the attributes of the methods """
+    """
+    instantiation of the attributes of the methods
+    Raising errors if coditons are not met
+    """
 
     def __init__(self, size=0):
         self.__size = size
@@ -23,4 +33,8 @@ class Square:
         self.__size = value
 
     def area(self):
+        """
+        Calculates area of square
+        Returns: area
+        """
         return (self.__size ** 2)
