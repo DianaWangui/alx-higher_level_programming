984c1b135c00593cda58979ae13bbc3f10950a71
DianaWangui <dianawanguikt048@gmail.com>
2023-11-28 23:40:08 +0300

    update readme

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+
+    """Return integer addition of a and b
+
+    if float a and b must first be casted to integer
+
+    if not integer or non-float :TypeEror.
+    """
+
+    if ((not isinstance (a, int) and not isinstance (b, float))):
+        raise TypeError ("a must be an integer")
+    if ((not isinstance (b, int) and not isinstance (b, float))):
+        raise TypeError ("b must be an integer")
+    return (int(a) + int(b)
