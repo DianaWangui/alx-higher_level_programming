963be1248e268915411dd3c8ea83c94d6a6a7a47
DianaWangui <dianawanguikt048@gmail.com>
2023-11-30 13:10:55 +0300

    pycode error

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -15,66 +15,65 @@ method def my_print(self): prints square using '#'
 
 
 class Square:
-  def __init__(self, size=0, position=(0, 0)):
-    """
-    square instance initialization
-    Args:
-      size: the size of square
-      position: position used to intend the square
-    """
-    self.__size = size
-    self.__position = position
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        square instance initialization
+        Args:
+            size: the size of square
+            position: position used to intend the square
+         """
+        self.__size = size
+        self.__position = position
 
-  @property
-  def  size(self):
-    """gets the size"""
-    return self.__size
-  
-  @size.setter
-  def size(self, value):
-    """sets the size"""
-    if not isinstance(value, int):
-      raise ValueError("size must be an integer")
-    elif value < 0:
-      raise ValueError("size must be >= 0")
-    self.__size = value
-  
-  @property
-  def position(self):
-    """gets position"""
-    return self.__position
-  
-  @position.setter
-  def position(self, value):
-    """setting position value"""
-    if type(value) is not tuple or len(value) != 2:
-      raise TypeError("position must be a tuple of 3 positive integer")
-    if not isinstance(value[0], int) or not isinstance(value[1], int):
-      raise TypeError("position must be a tuple of 2 positive integers")
-    if value[0] < 0 or value[1]:
-      raise ValueError("position must be a tuple of 2 positive integers")
-    self.__position = value
-  
-  def area(self):
-    """Finding area"""
-    return (self.__size **2)
-  
-  def my_print(self):
-    """printing square using symbol '#'."""
-    if self.size == 0:
-      print()
-    for i in range(self.__Position[1]):
-      print('')
-    for i in range(self.__size):
-      print("{}{}".format(' ' * self.__position[0], '#' * self.__size))
+    @property
+    def size(self):
+        """gets the size"""
+        return self.__size
 
-  def __str__(self):
-    """
-    prints a square offsetting it by position with symbol #
-    """
-    if self.__size == 0:
-      return ''
-    new_lines = '\n' * self.__position[1]
-    space = ' ' * self.__position[0]
-    hash = '#' * self.__size
-    return new_lines + '\n'.join(space + hash for e in range(self.size))
+    @size.setter
+    def size(self, value):
+        """sets the size"""
+        if not isinstance(value, int):
+            raise ValueError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """gets position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """setting position value"""
+        if type(value) is not tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 3 positive integer")
+        if not isinstance(value[0], int) or not isinstance(value[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1]:
+            raise ValueError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Finding area"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """printing square using symbol '#'."""
+        if self.size == 0:
+            print()
+        for i in range(self.__Position[1]):
+            print('')
+        for i in range(self.__size):
+            print("{}{}".format(' ' * self.__position[0], '#' * self.__size))
+
+    def __str__(self):
+        """
+        prints a square offsetting it by position with symbol #
+        """
+        if self.__size == 0:
+            return ''
+        new_lines = '\n' * self.__position[1]
+        space = ' ' * self.__position[0]hash = '#' * self.__size
+        return new_lines + '\n'.join(space + hash for e in range(self.size))
