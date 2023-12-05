a6e38f4b321fe90e3daae4deaebbd46639cd163b
DianaWangui <dianawanguikt048@gmail.com>
2023-12-06 00:12:26 +0300

    pycode error

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -18,5 +18,5 @@ class BaseGeometry:
     def integer_validation(self, name, value):
         if is not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
-        if value  <= 0:
+        if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
