73f8f491e73f55a2b537c5d135c210ebf91e774c
DianaWangui <dianawanguikt048@gmail.com>
2023-12-05 23:53:52 +0300

    empty gemetry class that defines a method area thats also empty

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""An empty class BaseGeometry with a public
+instance method area()
+"""
+
+
+class BaseGeometry:
+    """Base geometry class"""
+    def area(self):
+        """ an empty area function"""
+        raise Exception('area() is not implemented')
