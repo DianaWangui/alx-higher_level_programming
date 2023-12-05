f68756606327667f559fe076f01d08916dbb273e
DianaWangui <dianawanguikt048@gmail.com>
2023-12-06 00:59:07 +0300

    inheriting method from anothe class

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Dfining a class Rectangle that inherits
+"""Defining a class Rectangle that inherits
 from another class called BaseGeometry
 """
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
