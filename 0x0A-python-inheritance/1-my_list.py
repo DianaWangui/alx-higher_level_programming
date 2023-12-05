489c199c91cf4ffcb3a67f986704a5b6710c07ef
DianaWangui <dianawanguikt048@gmail.com>
2023-12-05 23:43:47 +0300

    creating using class oops

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -13,14 +13,3 @@ class MyList(list):
     def print_sorted(self):
         """printing the list in ascending order"""
         print(sorted(self))
-
-
-my_list = MyList()
-my_list.append(1)
-my_list.append(4)
-my_list.append(2)
-my_list.append(3)
-my_list.append(5)
-print(my_list)
-my_list.print_sorted()
-print(my_list)
