ebd54f69acbcc0d913df8dcc8f4fd1ab6de07ed7
DianaWangui <dianawanguikt048@gmail.com>
2023-12-05 23:31:29 +0300

    creating using class oops

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,6 +3,11 @@
 
 
 class MyList(list):
+    """instantiating the MyList attributes"""
+    def __init__(self):
+        """Calling the parent class list """
+        list.__init__(self):
+
     """public instance method for printing sorted list """
 
     def print_sorted(self):
