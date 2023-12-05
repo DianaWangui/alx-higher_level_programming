86e1aaab200295425cb8965ea763d97879ce3f06
DianaWangui <dianawanguikt048@gmail.com>
2023-12-05 23:13:34 +0300

    correcting tsask1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,7 +2,7 @@
 """Containing Mylist class """
 
 
-def Mylist(list):
+def MyList(list):
     """a list subclass """
     def __init__(self):
         """initilize the object """
@@ -11,3 +11,14 @@ def Mylist(list):
     def print_sorted(self):
         """printing the list in ascending order"""
         print(sorted(self))
+
+
+my_list = MyList()
+my_list.append(1)
+my_list.append(4)
+my_list.append(2)
+my_list.append(3)
+my_list.append(5)
+print(my_list)
+my_list.print_sorted()
+print(my_list)
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""an empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    pass
