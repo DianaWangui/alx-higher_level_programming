afefac44f9f20ba4fc83fdc5ab67b6bf81b039e5
DianaWangui <dianawanguikt048@gmail.com>
2023-12-10 02:13:12 +0300

    a function that creates an obj from json file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -4,6 +4,7 @@ import json
 
 
 def load_from_json_file(filename):
+    """creates a python obj from json file"""
     with open(filename, "r", encoding="utf-8") as json_file:
         json_data = json.load(json_file)
     return json_data
