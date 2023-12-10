039f1ff3f22a73ce1eea4757873cf60a9eaba93d
DianaWangui <dianawanguikt048@gmail.com>
2023-12-10 13:04:50 +0300

    pydocstyle error

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-"""a script that adds all args to a python list and save to file."""
+"""Add all arguments to a Python list and save them to file."""
 import sys
-import json
+
 
 save_to_json_file = __import__('5-save_to_json_file')
 load_from_json_file = __import__('6-load_from_json_file')
