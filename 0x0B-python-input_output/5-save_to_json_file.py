5f4b6a275e337d7c163a85a5c3a362756655ad98
DianaWangui <dianawanguikt048@gmail.com>
2023-12-11 21:40:03 +0300

    correcting a checker

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,8 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Write an object to a text file using JSON representation."""
-    obj_str = json.dumps(my_obj)
-
     with open(filename, "w") as file:
-        newfiles = file.write(obj_str)
-        return newfiles
+        json.dump(my_obj, file)
