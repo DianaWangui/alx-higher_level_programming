06a43838a71911d951762c9c64e2b7e65f4c5264
DianaWangui <dianawanguikt048@gmail.com>
2023-12-11 21:46:47 +0300

    a file that adds a list

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,16 +3,15 @@
 """Add all arguments to a Python list and save them to file."""
 import sys
 
+if __name__ = "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_jsaon_filr
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-save_to_json_file = __import__('5-save_to_json_file')
-load_from_json_file = __import__('6-load_from_json_file')
+    filename = "add_item.json"
 
-filename = "add_item.json"
-args = sys.argv
-
-
-with open(filename, "a+", encoding="utf-8") as file:
-    my_list = []
-    my_list.extend(args[1:])
+    try:
+        my_list= load_from_json_file(filename)
+    except FileNotFoundError:
+        my_list = []
+    my_list.extend(sys.args[1:])
     save_to_json_file(my_list, filename)
-    load_from_json_file(filename)
