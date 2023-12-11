aa7bfeadb1eaebe9b963b8298565c91add59d0b2
DianaWangui <dianawanguikt048@gmail.com>
2023-12-11 21:48:54 +0300

    a file that adds a list

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,11 +7,9 @@ if __name__ = "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_jsaon_filr
     load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-    filename = "add_item.json"
-
     try:
-        my_list= load_from_json_file(filename)
+        my_list= load_from_json_file("add_item.json")
     except FileNotFoundError:
         my_list = []
-    my_list.extend(sys.args[1:])
-    save_to_json_file(my_list, filename)
+    my_list.extend(sys.argv[1:])
+    save_to_json_file(my_list, "add_item.json")
