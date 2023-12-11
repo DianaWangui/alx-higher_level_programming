edd6d9bf6e7c37d650d210bec1229c489f8c69af
DianaWangui <dianawanguikt048@gmail.com>
2023-12-11 22:08:17 +0300

    a file that adds a list

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,6 +2,8 @@
 
 """Add all arguments to a Python list and save them to a file."""
 import sys
+import json
+
 
 if __name__ = "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
