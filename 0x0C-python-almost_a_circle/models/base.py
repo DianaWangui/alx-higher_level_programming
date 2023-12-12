ddcf4f80c782af493dba94a775771cdd7f42a8dd
DianaWangui <dianawanguikt048@gmail.com>
2023-12-13 00:15:56 +0300

    base mode file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,7 +3,9 @@
 class Base:
     __nb_objects = 0
     def __init__(self, id=None):
-        self.id = id
-    __nb_objects = 1
-    id - __nb_objects
+        if id is not None:
+            self.id = id
+            Base.__nb_objects += 1
+        else:
+            id - __nb_objects
 
