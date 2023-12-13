b959693c63544763ace714f5cb397eaea779f2d8
DianaWangui <dianawanguikt048@gmail.com>
2023-12-13 16:46:28 +0300

    creating a class rectangle that inherits from base

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+from models.base.py import Base
+"""Rectangle Class module that inherits from Base."""
 
 
 class Rectangle(Base):
+    """instantiating attributes of rectangle.
+    Inherits attribute of: id
+    from Base class.
+    """
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.__width = width
@@ -9,36 +15,54 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
 
+    # setting of getter and setter of each attribute
+    # width setter and getter
     @property
     def width(self):
+        """Getter for width
+        Return: width
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Setter for width"""
         self.__width = value
 
+    # height getter and setter
     @property
     def height(self):
+        """Getter for height
+        Return: height
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """setter for height"""
         self.height = value
 
+    # getter and setter for x
     @property
     def x(self):
+        """Getter for x
+        Return: x
+        """
         return self.__x
 
     @x.setter
     def x(self, value):
         self.__x = value
 
+    # Getter and Setter for y
     @property
     def y(self):
+        """Getter for y
+        Return: y
+        """
         return self.__y
 
     @y.setter
     def y(self, value):
+        """Setter for y"""
         self.__y = value
-
-
