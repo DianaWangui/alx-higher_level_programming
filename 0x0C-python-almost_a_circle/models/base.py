39569bfff8dc5180d60a62d8caa654c9f18b4535
DianaWangui <dianawanguikt048@gmail.com>
2023-12-13 15:25:29 +0300

    module documentation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""Base class with a public attribute and a class constructor."""
+
 
 class Base:
+    """private class attribute."""
     __nb_objects = 0
+    """instantiating a class constructor."""
     def __init__(self, id=None):
         if id is not None:
             self.id = id
