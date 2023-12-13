5ef243a5ea7d1c1c021ac802b242c5dde5a27031
DianaWangui <dianawanguikt048@gmail.com>
2023-12-13 19:02:27 +0300

    updating with method area

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -22,6 +22,7 @@ class Rectangle(Base):
             raise TypeError("height must be an integer")
         if height <= 0:
             raise ValueError("height must be > 0")
+        self.__height = height
 
         if type(x) != int:
             raise TypeError("x must be an integer")
