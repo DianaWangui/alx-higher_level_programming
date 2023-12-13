e93560828fd812c123aaffb4fe65f094cdb133c5
DianaWangui <dianawanguikt048@gmail.com>
2023-12-13 16:18:25 +0300

    rectangel

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
+
+
