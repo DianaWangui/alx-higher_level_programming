dae04b62bf87a2ea87c8c333b14b1113381e02da
DianaWangui <dianawanguikt048@gmail.com>
2023-12-14 14:07:26 +0300

    class method def create(cls, **dictionary): that returns an instance with all attributes already set

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -50,3 +50,16 @@ class Base:
             return []
 
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Create an instance using the provided dict."""
+        if cls.__name__ = 'Rectangle':
+            dummy_instance = cls(1,1)
+        elif cls.__name__ = 'Square':
+            dummy_instance = cls(1)
+        else:
+            raise NotImplementedError("Unsupported class")
+
+        dummy_instance.update(**dictionary)
+        return dummy_instance
