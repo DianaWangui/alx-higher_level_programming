5817ce9a287b209113263e7e8c4276ae3ef82ab9
DianaWangui <dianawanguikt048@gmail.com>
2023-12-14 11:03:47 +0300

    updating using *args method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -125,7 +125,14 @@ class Rectangle(Base):
 
     def update(self, *args):
         """Returning a string using *args method."""
-        if len(args) == 5:
-            self.id, self.__width, self.__height, self.__x, self.__y = args
-        else:
-            print("Error occured.")
+        if (args):
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
