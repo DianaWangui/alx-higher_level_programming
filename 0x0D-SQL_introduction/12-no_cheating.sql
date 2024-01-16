9d6647559795c41a8c035e9eb47cfd9c785e92da
DianaWangui <dianawanguikt048@gmail.com>
2024-01-16 19:54:26 +0300

    update bobs score

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -3,4 +3,4 @@
 -- You are not allowed to use Bob’s id value, only the name field
 -- The database name will be passed as an argument of the mysql command
 
-UPDATE second_table SET score = 10 WHERE id = 3;
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
