dc45239edc06e93b3f1db7809707b8802aa941be
DianaWangui <dianawanguikt048@gmail.com>
2024-01-16 19:52:19 +0300

    update bobs score

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,6 @@
+-- Write a script that updates the score of Bob to 10 in the table second_table.
+
+-- You are not allowed to use Bob’s id value, only the name field
+-- The database name will be passed as an argument of the mysql command
+
+UPDATE second_table SET score = 10 WHERE id = 3;
