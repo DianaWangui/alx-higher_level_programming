89abb7b6d655a62d7f6b4962908dd17d53815166
DianaWangui <dianawanguikt048@gmail.com>
2024-01-23 22:53:52 +0300

    update task 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,8 @@
 -- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 
-USE hbtn_0d_tvshows;
+-- CREATE DATABASE IF NOT EXISTS hbtn_0d_tvshows;
+
+-- USE hbtn_0d_tvshows;
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows INNER JOIN tv_show_genres
