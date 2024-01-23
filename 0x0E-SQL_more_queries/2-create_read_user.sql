48004a13d86088985933b9bba79780ddaf60ba81
DianaWangui <dianawanguikt048@gmail.com>
2024-01-23 14:39:13 +0300

    update task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
--- script that creates the database hbtn_0d_2 and the user user_0d_2
-CREATE DATABASES IF NOT EXISTS hbtn_0d_2;
+-- script that creates the database hbtn_0d_2 and the user_0d_2 user
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- Create User
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
