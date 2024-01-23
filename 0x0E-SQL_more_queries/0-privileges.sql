0790ee90d386eabba005e2173424089beea9c804
DianaWangui <dianawanguikt048@gmail.com>
2024-01-23 13:24:22 +0300

    task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- script that lists all privileges of the MySQL users
 --user_0d_1 and user_0d_2 on your server (in localhost).
 
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
