1856c399705c86e1f94ca065c4b88b7cbe77f42f
DianaWangui <dianawanguikt048@gmail.com>
2024-01-24 16:01:39 +0300

    update task 11

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,8 +1,7 @@
 -- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 
-SELECT tv_shows.title, COALESCE(tv_show_genres.genre_id, 'NULL') AS genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC,
-COALESCE(tv_show_genres.genre_id, 'NULL');
+RIGHT JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title tv_show_genres.genre_id ASC 
