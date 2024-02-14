db0ec338c7000b0dbe04634fc557749bc509fd65
DianaWangui <dianawanguikt048@gmail.com>
2024-02-14 20:49:29 +0300

    class Square that defines a square and inherits from Square of 5-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const SquareBase = require('./5-square');
+class Square extends SquareBase {
+  charPrint(c) {
+    if (!c) c = 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+module.exports = Square;
