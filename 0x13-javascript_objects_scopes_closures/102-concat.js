873074b5958b550fb814e8e86f76a4e30fd6ee90
DianaWangui <dianawanguikt048@gmail.com>
2024-02-14 21:25:28 +0300

    Write a script that concats 2 files.

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const fs = require('fs').promises;
+const { argv } = require('process');
+const fileA = argv[2];
+const fileB = argv[3];
+const fileC = argv[4];
+async function concatTwoFiles() {
+  try {
+    const data1 = await fs.readFile(fileA);
+    const data2 = await fs.readFile(fileB);
+    fs.writeFile(fileC, data1 + data2);
+  } catch (err) {
+    console.log(err);
+  }
+}
+concatTwoFiles();
