b120535ab6f95a8acf4bf193429eabb6d74fff11
DianaWangui <dianawanguikt048@gmail.com>
2024-03-13 20:54:49 +0300

    update with semistardard error validation check

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const SquareBase = require('./5-square');
 class Square extends SquareBase {
-  charPrint(c) {
+  charPrint (c) {
     if (!c) c = 'X';
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
