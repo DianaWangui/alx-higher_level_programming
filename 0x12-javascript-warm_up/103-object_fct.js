804fda2640c5c03c34e4ae3c6132dcd2462e59cf
DianaWangui <dianawanguikt048@gmail.com>
2024-03-13 21:12:31 +0300

    update

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 const myObject = {
   type: 'object',
-  value: 12,
-  incr: function () {
+  value: 12
+}
+  myObject.incr = function () {
     this.value++;
-  }
 };
+
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
