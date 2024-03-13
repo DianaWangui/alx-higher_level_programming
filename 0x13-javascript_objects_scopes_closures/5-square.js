c7e74f8e1d26cee4002139a208117bebeab033f0
DianaWangui <dianawanguikt048@gmail.com>
2024-03-13 20:43:46 +0300

    update with semistardard error validation check

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -2,7 +2,7 @@
 const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
   constructor (size) {
-    super(size, size);
+    super (size, size);
   }
 }
 module.exports = Square;
