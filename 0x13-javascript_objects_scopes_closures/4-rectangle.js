fe456a835da395050d5e8416970011689c7eb801
DianaWangui <dianawanguikt048@gmail.com>
2024-03-13 20:37:05 +0300

    update to remove checker error

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -6,17 +6,17 @@ class Rectangle {
       this.height = h;
     }
   }
-  print() {
+  print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
-  rotate() {
+  rotate () {
     const temp = this.width;
     this.width = this.height;
     this.height = temp;
   }
-  double() {
+  double () {
     this.width *= 2;
     this.height *= 2;
   }
