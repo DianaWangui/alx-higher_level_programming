85b5aaadea2d89c33fa762f6fde36a8c7470508e
DianaWangui <dianawanguikt048@gmail.com>
2024-03-18 21:35:46 +0300

    pycodestyle error correction

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,6 +4,8 @@
 
 import MySQLdb
 import sys
+
+
 def list_states(username, password, database):
     """Establish a connection to the databse server."""
     db = MySQLdb.connect(
@@ -14,24 +16,25 @@ def list_states(username, password, database):
         port=3306
     )
 
-    #Creating a cursor in order to give the connection a good use
+    # Creating a cursor
     cursor = db.cursor()
 
-    #executing the cursor in order to sort all states starting with N
+    # executing the cursor in order to sort all states starting with N
     cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     states = cursor.fetchall()
 
-    #printing th states
+    # printing th states
     for state in states:
         print(state)
 
-    #closing the db and cursor
+    # closing the db and cursor
     cursor.close()
     db.close()
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: python script.py <mysql_username> <mysql_password> <database_name>")
+        print("Usage: python script.py")
         sys.exit(1)
     username = sys.argv[1]
     password = sys.argv[2]
