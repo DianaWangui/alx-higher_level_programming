468dffa793579aab751c661198106fa499014255
DianaWangui <dianawanguikt048@gmail.com>
2024-03-20 11:31:25 +0300

    sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""Script that lists all states from the database."""
+
+import MySQLdb
+import sys
+
+
+def list_states(username, password, database, argument1):
+    """Establish a connection to the databse server."""
+    db = MySQLdb.connect(
+        host='localhost',
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306,
+    )
+
+    cur = db.cursor()
+    sql_query = 'SELECT * FROM states WHERE BINARY\
+    name = "{}" ORDER BY id asc'.format(argument1)
+    cur.execute(sql_query)
+
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: python script.py <username> \
+        <password> <database> <argument1>")
+        sys.exit(1)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    argument1 = sys.argv[4]
+
+    list_states(username, password, database, argument1)
