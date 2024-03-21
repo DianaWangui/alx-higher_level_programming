c9e604e90891654a1ef42ab72103c4f6437e831d
DianaWangui <dianawanguikt048@gmail.com>
2024-03-21 20:10:08 +0300

    update

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+def states_list(username, password, state_name):
+    db = MySQLdb.connect(
+        username=username,
+        passwd=password,    
+    )
