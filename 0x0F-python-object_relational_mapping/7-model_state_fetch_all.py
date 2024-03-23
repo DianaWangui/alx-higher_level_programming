2aaf8ee9584e72eb4ce906c1c3aef9e5f48a7f86
DianaWangui <dianawanguikt048@gmail.com>
2024-03-23 22:48:21 +0300

    python file that contains the class definition of a State and an instance Base = declarative_base():

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+"""script that lists all State objects from the database hbtn_0e_6_usa."""
+
 import sys
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
