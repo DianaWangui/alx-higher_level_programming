d94b86ab6beab739a4a207151f6425c285e4ed6d
DianaWangui <dianawanguikt048@gmail.com>
2024-03-23 21:54:40 +0300

    python file that contains the class definition of a State and an instance Base = declarative_base():

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,7 +2,7 @@
 """python file that contains the class definition of a State."""
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 
 
 Base = declarative_base()
