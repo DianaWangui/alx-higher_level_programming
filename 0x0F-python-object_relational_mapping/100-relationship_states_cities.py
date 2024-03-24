a8c075d205eaed81a67a4e6fa16483e14e48a6e0
DianaWangui <dianawanguikt048@gmail.com>
2024-03-24 13:33:10 +0300

    pycode error

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         ),
         pool_pre_ping=True,
     )
-    
+
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
