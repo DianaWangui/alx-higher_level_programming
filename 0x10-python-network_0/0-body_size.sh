57095b0710106c088677c2803ac8308de6fc43c4
DianaWangui <dianawanguikt048@gmail.com>
2024-03-30 14:18:45 +0300

    script sends a request to a URL and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script sends a request to a URL and displays the size of the body of the response
+# This script sends a request to a URL and displays the size
 curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
