c65577b1eb070290ba0ef55b91d142ff4641e004
DianaWangui <dianawanguikt048@gmail.com>
2024-03-31 14:04:45 +0300

    Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl curl -s -o /dev/null -w "%{http_code}" "$1"
+curl -s -o /dev/null -w "%{http_code}" "$1"
