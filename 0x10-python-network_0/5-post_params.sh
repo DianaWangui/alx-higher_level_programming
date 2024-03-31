b52894938a32c8c04cdebdbc98c5daa6e7431078
DianaWangui <dianawanguikt048@gmail.com>
2024-03-31 13:17:08 +0300

    Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a GET request to the URL with the header variable X-School-User-Id set to 98
-curl -sL POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
