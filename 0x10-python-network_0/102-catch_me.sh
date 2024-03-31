b49ad5c7d97f61afe6e068f9a0a6645c38ac2ebf
DianaWangui <dianawanguikt048@gmail.com>
2024-03-31 14:30:09 +0300

    changing the script

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Make a request to 0.0.0.0:5000/catch_me and display the body of the response
-curl -s -X PUT -L -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me -o /dev/null
+curl -sLX PUT 0.0.0.0:5000/catch_me -H "Origin: School"  -d "user_id=98"
