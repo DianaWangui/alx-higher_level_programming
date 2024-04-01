1a7a0663d0feb013a532dc0d15617d22cf12096a
DianaWangui <dianawanguikt048@gmail.com>
2024-04-01 22:12:34 +0300

    update with no redudant print statement

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,5 +6,5 @@ with urllib.request.urlopen(url) as req:
     html_content = req.read()
     utf_data = html_content.decode('utf-8')
     data_type = type(html_content)
-    print(f'Body response:\n\t- type: {data_type}\n\t- \
-    content{html_content}\n\t- utf8 content: {utf_data}')
+    print(f'Body response:\n\t- type\
+: {data_type}\n\t- content: {html_content}\n\t- utf8 content: {utf_data}')
