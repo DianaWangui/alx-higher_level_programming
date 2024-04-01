7a401baf6ff498ad10bf0977692cb3ecf69a3ffe
DianaWangui <dianawanguikt048@gmail.com>
2024-04-01 21:42:08 +0300

    update with module docs

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-""" module doc """
+""" Python script that takes in a URL, sends a request to the URL. """
 import urllib.request
 import sys
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    with urllib.request.urlopen(url) as response:
-        print(response.info()["X-Request-Id"])
+url = sys.argv[1]
+with urllib.request.urlopen(url) as req:
+    headers = req.headers
+    x_request_id = headers.get('X-Request-Id')
+    print(x_request_id)
