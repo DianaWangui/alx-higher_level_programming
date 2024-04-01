0f8a3576a7b718982b8510e12b8d7762f38fd664
DianaWangui <dianawanguikt048@gmail.com>
2024-04-01 20:44:57 +0300

    Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# A script that fetches url
+import urllib.request
+url = 'https://alx-intranet.hbtn.io/status'
+with urllib.request.urlopen(url) as req:
+    html_content = req.read()
+    print("Body response:")
+    print("\t- type:", type(html_content))
+    print("\t- content:", html_content)
+    print("\t- utf8 content:", html_content.decode('utf-8'))
+    
