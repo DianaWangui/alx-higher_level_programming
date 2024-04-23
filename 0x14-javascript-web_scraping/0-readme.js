3d2c7266798f42fdcaed26231f436fea5174ccba
DianaWangui <dianawanguikt048@gmail.com>
2024-04-24 00:08:16 +0300

    Added: a script that reads and prints the content of a file.

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log(data);
+    }
+})
