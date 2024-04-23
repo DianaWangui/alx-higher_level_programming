1c2c60353ff0e2a16951870987727f55768c50f5
DianaWangui <dianawanguikt048@gmail.com>
2024-04-24 00:09:33 +0300

    Added: Fixed semistandard error

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,10 +3,9 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-        console.log(err);
-    }
-    else {
-        console.log(data);
-    }
-})
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
