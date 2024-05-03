10e62ef969ca0f2e4dde425440b5193be72ec54d
DianaWangui <dianawanguikt048@gmail.com>
2024-05-03 13:14:02 +0300

    JavaScript script that updates the text color of the <header> element to red (#FF0000):JavaScript script that updates the text color of the <header> element to red (#FF0000):

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const my_header = document.querySelector("header");
+    if (my_header) {
+        my_header.style.color = "#FF0000";
+    }
+});
